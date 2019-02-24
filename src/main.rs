67e23902ebcba25e78617596bb1817b073f16d9e
ToRainu256 <torainu256@gmail.com>
2019-02-24 15:03:58 +0900

    足し算と引き算を追加

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,49 @@
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let input: i32 = args[1].parse().unwrap();
+extern crate r9cc;
+use r9cc::strtol;
 
+fn main() {
+    //標準入力を受け取り、引数の個数をチェック
+    let mut args = std::env::args();
+    //println!("{:?}", args);
+    if args.len() != 2 {
+        eprint!("Usage: 9cc <code>\n");
+        return;
+    }
+    //nth(n)はn番目の文字列を取り出す
+    let p = args.nth(1).unwrap();
+
+    //最初のおまじない
+    //INTEL記法のアセンブリを使うことを明示したあと
+    //MAIN関数を定義する
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("    mov rax, {}", input);
+
+    let (n, mut p) = strtol(&p); //C言語のstrtolに相当文字列をlong型の整数に変換する
+    println!("    mov rax, {}", n.unwrap());
+    //自作のstrtolは引数の変換した部分を切り取ったものを返す
+    //だから、０番目から舐める。
+    //
+    while let Some(c) = p.chars().nth(0) {
+        let s = p.split_off(1); //Vecのｎ番目以降を切り落とす。
+
+        if c == '+' {
+            let (n, remaining) = strtol(&s);
+            p = remaining;
+            println!("    add rax, {}", n.unwrap());
+            continue;
+        }
+
+        if c == '-' {
+            let (n, remaining) = strtol(&s);
+            p = remaining;
+            println!("    sub rax, {}", n.unwrap());
+            continue;
+        }
+
+        eprint!("unexpected character: {}\n", p);
+        return;
+    }
     println!("    ret");
+    return;
 }
